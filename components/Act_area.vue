787e9cb752ac07c53155d620dca612c1ec5913af
DESKTOP-47A4PN7\1 <jason86100110@gmail.com>
2026-01-19 17:03:20 +0800

    調整以及製作好活動訊息區塊組件。

--- components/Act_area.vue
+++ components/Act_area.vue
@@ -1,73 +1,77 @@
 <template>
   <section class="act-area">
-    <div class="act-area__container">
-      <div class="act-area__content">
-        <Act_title type="rules" id="activity" />
-        <div class="act-area__rules">
-          <h3>活動時間：</h3>
-          <h4>2025 / 09 / 22 ( 一 ) 10:00 ~ 2025 / 10 / 14 ( 二 ) 10:00為止</h4>
-          <h3>中獎公布：</h3>
-          <h4>2025 / 10 / 23 ( 四 )</h4>
-          <p>活動說明：</p>
-          <ul>
-            <li>活動限會員身分參加，需登 ( 加 ) 入會員。</li>
-            <li>
-              點擊摩天輪上的
-              「立即占卜」，即可開始預測；占卜預測完成，即可獲得抽獎資格。
-            </li>
-            <li>
-              活動期間，每個會員不限占卜次數，每天皆可占卜（每日從00:00起算），累計完成占卜次數，則可抽取指定獎項。
-            </li>
-          </ul>
-        </div>
-        <Act_title type="info" />
-        <div class="act-area__info">
-          <ul class="act-area__info-list">
-            <li class="act-area__info-list-items">
-              LINE Points 5 點將於 2025/10/27 起兩周內，以隨機方式透過 e-mail
-              或簡訊方式陸續派發兌換序號。請參加者於 2025/10/24
-              前，至會員中心填妥您的 e-mail
-              和手機號碼等領獎資料。若逾期未填或兩者缺一，聯合線上得取消其得獎資格。
-            </li>
-            <li class="act-area__info-list-items">
-              各獎項（獎品圖示僅供參考）將於活動結束後，由主辦單位以系統隨機抽出，並以
-              e-mail
-              或簡訊方式通知領取，請務必確認填寫資訊的真實性與正確性皆無誤。
-            </li>
-            <li class="act-area__info-list-items">
-              主辦單位保留隨時修正、暫停、終止或解釋本活動之最終權利。
-            </li>
-            <li class="act-area__info-list-items">
-              若發現人為操作、使用網頁機器人程式或一次性信箱等參與活動，聯合線上得取消其參加或得獎資格。
-            </li>
-            <li class="act-area__info-list-items">
-              本活動獲獎資格不得重複，每人僅限一次領獎機會（依個人資料為準），均不得重複領獎。
-            </li>
-            <li class="act-area__info-list-items">
-              若經查核發現，得獎者於活動開獎日前一年內，參與聯合線上活動累計中獎獎項價值超過新台幣
-              1,000 元（含）以上，將取消本活動之獲獎資格。
-            </li>
-            <li class="act-area__info-list-items">
-              若使用 Gmail
-              帳號參加活動，於帳號任意字元間加入小數點，均視為同一個信箱，僅有一次獲（抽）獎資格。例如，已有
-              123@gmail.com 參加活動，將主動排除 1.23@gmail.com、12.3@gmail.com
-              等變體信箱。
-            </li>
-            <li class="act-area__info-list-items">
-              參與本次活動即代表您已充分了解並同意接受個資聲明與注意事項。
-            </li>
-          </ul>
-        </div>
+    <h1 class="act-area__title">
+      <img src="/imgs/activity_title.png" alt="活動標題" />
+    </h1>
+    <Act_container>
+      <div class="act-area__rules">
+        <h3>活動時間：</h3>
+        <h4>2025 / 09 / 22 ( 一 ) 10:00 ~ 2025 / 10 / 14 ( 二 ) 10:00為止</h4>
+        <h3>中獎公布：</h3>
+        <h4>2025 / 10 / 23 ( 四 )</h4>
+        <p>活動說明：</p>
+        <ul>
+          <li>活動限會員身分參加，需登 ( 加 ) 入會員。</li>
+          <li>
+            點擊摩天輪上的
+            「立即占卜」，即可開始預測；占卜預測完成，即可獲得抽獎資格。
+          </li>
+          <li>
+            活動期間，每個會員不限占卜次數，每天皆可占卜（每日從00:00起算），累計完成占卜次數，則可抽取指定獎項。
+          </li>
+        </ul>
       </div>
-    </div>
-    <Act_title type="prize" id="prizes" />
+    </Act_container>
+    <h1 class="act-area__title">
+      <img src="/imgs/info_title.png" alt="活動標題" />
+    </h1>
+    <Act_container>
+      <div class="act-area__info">
+        <ul class="act-area__info-list">
+          <li class="act-area__info-list-items">
+            LINE Points 5 點將於 2025/10/27 起兩周內，以隨機方式透過 e-mail
+            或簡訊方式陸續派發兌換序號。請參加者於 2025/10/24
+            前，至會員中心填妥您的 e-mail
+            和手機號碼等領獎資料。若逾期未填或兩者缺一，聯合線上得取消其得獎資格。
+          </li>
+          <li class="act-area__info-list-items">
+            各獎項（獎品圖示僅供參考）將於活動結束後，由主辦單位以系統隨機抽出，並以
+            e-mail
+            或簡訊方式通知領取，請務必確認填寫資訊的真實性與正確性皆無誤。
+          </li>
+          <li class="act-area__info-list-items">
+            主辦單位保留隨時修正、暫停、終止或解釋本活動之最終權利。
+          </li>
+          <li class="act-area__info-list-items">
+            若發現人為操作、使用網頁機器人程式或一次性信箱等參與活動，聯合線上得取消其參加或得獎資格。
+          </li>
+          <li class="act-area__info-list-items">
+            本活動獲獎資格不得重複，每人僅限一次領獎機會（依個人資料為準），均不得重複領獎。
+          </li>
+          <li class="act-area__info-list-items">
+            若經查核發現，得獎者於活動開獎日前一年內，參與聯合線上活動累計中獎獎項價值超過新台幣
+            1,000 元（含）以上，將取消本活動之獲獎資格。
+          </li>
+          <li class="act-area__info-list-items">
+            若使用 Gmail
+            帳號參加活動，於帳號任意字元間加入小數點，均視為同一個信箱，僅有一次獲（抽）獎資格。例如，已有
+            123@gmail.com 參加活動，將主動排除 1.23@gmail.com、12.3@gmail.com
+            等變體信箱。
+          </li>
+          <li class="act-area__info-list-items">
+            參與本次活動即代表您已充分了解並同意接受個資聲明與注意事項。
+          </li>
+        </ul>
+      </div>
+    </Act_container>
+
     <Prize />
   </section>
 </template>
 
 <script setup>
-import Act_title from "./Act_title.vue";
 import Prize from "./Prize.vue";
+import Act_container from "./Act_container.vue";
 defineProps({
   name: "ActArea",
 });
@@ -98,30 +102,24 @@ onMounted(() => {
 .act-area {
   // border: 1px solid #ccc;
   padding: 0 12px;
-
-  .act-area__container {
-    border: 3px solid #d8ceff;
-    border-radius: 50px;
-    box-shadow: inset 0 0 50px 20px rgba(#8d46d6, 0.5);
-    background-color: rgba($color: #8d46d6, $alpha: 0.3);
-    max-width: 1200px;
-    margin: 0 auto;
-    padding: 0 12px;
-    margin-bottom: 140px;
-    padding-top: 120px;
-    @media (max-width: 1024px) {
-      padding-top: 80px;
-      margin-bottom: 80px;
+  .act-area__title {
+    max-width: 650px;
+    max-height: 75px;
+    margin: 0 auto 40px auto;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 120px;
+    @media (max-width: 768px) {
+      max-width: 520px;
+      max-height: 60px;
     }
-    @media (max-width: 480px) {
-      padding-top: 40px;
-      margin-bottom: 40px;
+    img {
+      width: 100%;
+      max-height: 75px;
+      @media (max-width: 768px) {
+        max-height: 60px;
+      }
     }
-  }
-  .act-area__content {
-    // border: 1px solid green;
-    max-width: 800px;
-    margin: 0 auto;
   }
   .act-area__rules {
     // border: 1px solid yellow;
---
+++ components/Act_container.vue
@@ -0,0 +1,84 @@
+<template>
+  <section class="act-container">
+    <div class="act-container__border-top">
+      <img src="/imgs/act_container_top.png" alt="活動邊框" />
+    </div>
+    <div class="act-content">
+      <slot></slot>
+    </div>
+    <div class="act-container__border-bottom">
+      <img src="/imgs/act_container_bottom.png" alt="活動邊框" />
+    </div>
+  </section>
+</template>
+<script setup>
+defineProps({
+  name: "ActContainer",
+});
+</script>
+<style lang="scss" scoped>
+.act-container {
+  container-type: inline-size;
+  border-right: 6px solid #faebb4;
+  border-left: 6px solid #faebb4;
+  background-color: #ee772b;
+  max-width: 1280px;
+  margin: 0 auto;
+  margin-bottom: 180px;
+
+  &__border-top {
+    width: 100%;
+    position: relative;
+    img {
+      position: relative;
+      margin-top: -6.3cqw;
+      margin-left: -0.6cqw;
+      width: 101.3cqw;
+      height: auto;
+      display: block;
+    }
+  }
+
+  &__border-bottom {
+    width: 100%;
+    position: relative;
+    img {
+      position: relative;
+      margin-left: -0.5cqw;
+      margin-bottom: -6.1cqw;
+      width: 101cqw;
+      height: auto;
+      display: block;
+    }
+  }
+
+  @media (max-width: 1024px) {
+    margin-bottom: 80px;
+    border-right: 5px solid #faebb4;
+    border-left: 5px solid #faebb4;
+  }
+  @media (max-width: 850px) {
+    border-right: 4px solid #faebb4;
+    border-left: 4px solid #faebb4;
+  }
+  @media (max-width: 768px) {
+    border-right: 3px solid #faebb4;
+    border-left: 3px solid #faebb4;
+  }
+  @media (max-width: 480px) {
+    margin-bottom: 40px;
+    border-right: 2px solid #faebb4;
+    border-left: 2px solid #faebb4;
+  }
+  @media (max-width: 375px) {
+    border-right: 1.5px solid #faebb4;
+    border-left: 1.5px solid #faebb4;
+  }
+}
+
+.act-content {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 0 12px;
+}
+</style>
